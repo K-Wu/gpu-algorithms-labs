74447507da31a7b2474b55cf4c363129307d398a
K-Wu <11816012+K-Wu@users.noreply.github.com>
2021-09-06 01:01:36 -0500

    finish sgemm-regtiled-coarsened

--- labs/sgemm-regtiled-coarsened/template.cu
+++ labs/sgemm-regtiled-coarsened/template.cu
@@ -26,8 +26,55 @@ __global__ void mysgemm(int m, int n, int k, const float *A, const float *B, flo
   #define A(row,col) A[(row) + (col)*m]
   #define B(row,col) B[(row)*n + (col)]
   #define C(row,col) C[(row) + (col)*m]
-
+  __shared__ float shmem[TILE_SZ_RATIO][TILE_SZ_B];
   // INSERT KERNEL CODE HERE
+
+  int ArowIdx = blockIdx.x*TILE_SZ_A + threadIdx.x;
+  
+  for (int i = 0; i < (k+TILE_SZ_RATIO-1)/TILE_SZ_RATIO; i++) {
+    // load A in registers
+    float reg0=0.0f;
+    float reg1=0.0f;
+    float reg2=0.0f;
+    float reg3=0.0f;
+    float reg4=0.0f;
+    float reg5=0.0f;
+    float reg6=0.0f;
+    float reg7=0.0f;
+    if (ArowIdx < m) {
+    reg0 = (k>i*TILE_SZ_RATIO)?A(ArowIdx,i*TILE_SZ_RATIO):0.0f;
+    reg1 = (k>i*TILE_SZ_RATIO+1)?A(ArowIdx,i*TILE_SZ_RATIO+1):0.0f;
+    reg2 = (k>i*TILE_SZ_RATIO+2)?A(ArowIdx,i*TILE_SZ_RATIO+2):0.0f;
+    reg3 = (k>i*TILE_SZ_RATIO+3)?A(ArowIdx,i*TILE_SZ_RATIO+3):0.0f;
+    reg4 = (k>i*TILE_SZ_RATIO+4)?A(ArowIdx,i*TILE_SZ_RATIO+4):0.0f;
+    reg5 = (k>i*TILE_SZ_RATIO+5)?A(ArowIdx,i*TILE_SZ_RATIO+5):0.0f;
+    reg6 = (k>i*TILE_SZ_RATIO+6)?A(ArowIdx,i*TILE_SZ_RATIO+6):0.0f;
+    reg7 = (k>i*TILE_SZ_RATIO+7)?A(ArowIdx,i*TILE_SZ_RATIO+7):0.0f;
+    }
+    // load B in shared memory
+    int shdmemLDBrowIdx = i*TILE_SZ_RATIO+threadIdx.x/TILE_SZ_B;
+    int shdmemLDBcolIdx =blockIdx.y*TILE_SZ_B + threadIdx.x%TILE_SZ_B;
+    shmem[threadIdx.x/TILE_SZ_B][threadIdx.x%TILE_SZ_B] = (shdmemLDBrowIdx<k && shdmemLDBcolIdx<n)?B(shdmemLDBrowIdx,shdmemLDBcolIdx):0.0f;
+    
+    __syncthreads();
+    // compute C
+    if (ArowIdx<m) {
+        for (int shdmemColIdx=0;shdmemColIdx<TILE_SZ_B;shdmemColIdx++) {
+            int CcolIdx=shdmemColIdx+blockIdx.y*TILE_SZ_B;
+            if (CcolIdx<n) {
+                C(ArowIdx, CcolIdx)+=reg0*shmem[0][shdmemColIdx];
+                C(ArowIdx, CcolIdx)+=reg1*shmem[1][shdmemColIdx];
+                C(ArowIdx, CcolIdx)+=reg2*shmem[2][shdmemColIdx];
+                C(ArowIdx, CcolIdx)+=reg3*shmem[3][shdmemColIdx];
+                C(ArowIdx, CcolIdx)+=reg4*shmem[4][shdmemColIdx];
+                C(ArowIdx, CcolIdx)+=reg5*shmem[5][shdmemColIdx];
+                C(ArowIdx, CcolIdx)+=reg6*shmem[6][shdmemColIdx];
+                C(ArowIdx, CcolIdx)+=reg7*shmem[7][shdmemColIdx];
+            }
+        }
+    }
+    __syncthreads();
+  }
 
 }
 
@@ -54,11 +101,12 @@ void basicSgemm(char transa, char transb, int m, int n, int k, float alpha, cons
     }
 
     // Initialize thread block and kernel grid dimensions ---------------------
-
+    dim3 blocks((m+TILE_SZ_A-1)/TILE_SZ_A, (n+TILE_SZ_B-1)/TILE_SZ_B);
+    dim3 threadsPerBlock(TILE_SZ_A);
     //INSERT CODE HERE
-
+    cudaMemset(C, 0, m*n*sizeof(float));
     // Invoke CUDA kernel -----------------------------------------------------
-
+    mysgemm<<<blocks,threadsPerBlock>>>(m, n, k, A, B, C);
     //INSERT CODE HERE
 
 }
